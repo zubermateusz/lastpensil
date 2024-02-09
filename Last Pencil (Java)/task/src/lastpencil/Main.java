15e4ccdd907e9b18660a88a45df18a04e7041e28
Mateusz <zubermateusz@gmail.com>
2024-02-09 21:06:04 +0100

    Stage 2/5: New rules

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,17 +1,25 @@
 package lastpencil;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random random = new Random();
-        int licznik = random.nextInt(7);
-        while (licznik > 0) {
+
+        System.out.println("How many pencils would you like to use:"); // ask user
+        Scanner scanner = new Scanner(System.in);
+        int pencils = scanner.nextInt();// read how many pencils
+        System.out.println("Who will be the first (John, Jack)");
+        String player1 = scanner.nextLine(); // read line after int
+        player1 = scanner.nextLine();
+        //Random random = new Random();
+        //int pencils = random.nextInt(7);
+        while (pencils > 0) {
             System.out.print("|");
-            licznik--;
+            pencils--;
         }
         System.out.println();
-        System.out.println("Your turn!");
+        System.out.println(player1 + " is going first!");
 
     }
 }
