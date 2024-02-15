478a6e79e6c49235ed174af395bbcb673f56efdb
Mateusz <zubermateusz@gmail.com>
2024-02-15 21:29:47 +0100

    Stage 5/5: The right strategy finished

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -2,6 +2,7 @@ package lastpencil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -19,7 +20,7 @@ public class Main {
         System.out.println("How many pencils would you like to use:"); // ask user
         Scanner scanner = new Scanner(System.in);
         int pencils = 0;
-        while (pencils == 0) {
+        while (pencils == 0) {// read correct count of pencils from user
             try {
                 pencils = Integer.parseInt(scanner.nextLine());// read how many pencils start
             } catch (NumberFormatException e) {
@@ -36,13 +37,14 @@ public class Main {
 
         int player1 = 2;// temporary set player1 to id not used
         System.out.println("Who will be the first (" + playersNames.get(0) + ", " + playersNames.get(1) + "):");
-        while (player1 == 2) {
+        while (player1 == 2) {// read correct name of player from user
             switch (scanner.nextLine()) { // read who started
                 case ("John") -> player1 = 0;
                 case ("Jack") -> player1 = 1;
                 default -> System.out.println("Choose between '" + playersNames.get(0) + "' and '" + playersNames.get(1) + "'");
             }
         }
+
         //start the game
         printPencils(pencils);// print pencils in one line
 
@@ -51,7 +53,35 @@ public class Main {
             int tempPencils = pencils;
             for(;;) {
                 try {
-                    tempPencils = Integer.parseInt(scanner.nextLine());
+                    if (player1 == 1) {
+                        if (pencils == 1) {tempPencils = 1;
+                            System.out.println(tempPencils);}
+                        else {
+                            if (pencils % 4 == 0) {
+                                tempPencils = 3;
+                                System.out.println(tempPencils);
+                            } //4,8,12,16,...
+                            if ((pencils + 1) % 4 == 0) {
+                                tempPencils = 2;
+                                System.out.println(tempPencils);
+                            } // 3,7,11,15,...
+                            if ((pencils + 2) % 4 == 0) {
+                                tempPencils = 1;
+                                System.out.println(tempPencils);
+                            } //2,6,10,14,...
+                            if ((pencils - 1) % 4 == 0) {
+                                tempPencils = new Random().nextInt(3) + 1;
+                                System.out.println(tempPencils);// 5,9,13,17,...
+                            }
+                        }
+
+
+                    }
+                    else {
+                        tempPencils = Integer.parseInt(scanner.nextLine());
+                    }
+
+
                     if (tempPencils >= 1 && tempPencils <= 3) {
                         if (tempPencils > pencils) {
                             System.out.println("Too many pencils were taken");
@@ -66,22 +96,26 @@ public class Main {
 
                             System.out.println(playersNames.get(player1) + " won!");
                             break;
+
                         } else {
                             printPencils(pencils);// print pencils in one line
                             break;
                         }
                     } else {
-                        throw  new NumberFormatException(); // if tempPencils != 1,2,3
+                        throw new NumberFormatException(); // if tempPencils != 1,2,3
                     }
                 } catch (NumberFormatException e) {
                     System.out.println("Possible values: '1', '2' or '3'");
                 }
             }
 
-            player1++; //select next player name
+            player1++; //select player name for round
             if (player1 >= playersNames.size()) {
                 player1 = 0; // change on list[0]
             }
         }
+
+
+
     }
 }
